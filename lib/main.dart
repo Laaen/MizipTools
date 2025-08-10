8b645ba5f2f2e770c13eb7021b8a4ff12bf0a3da
Laen <Laaen@hotmail.fr>
2025-08-10 23:15:33 +0200

    Refactoring : Nettoyage du code de main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:miziptools/misc/mifare_classic_tag.dart';
 
 import "pages/main_page.dart";
 import 'package:logging/logging.dart';
@@ -7,29 +8,31 @@ import 'package:logging/logging.dart';
 // TODO: Tag OK, balance: 4.92
 
 void main() {
+  setupLogging();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const App());
+}
 
-  // Setup logging
+void setupLogging(){
   Logger.root.level = Level.INFO;
   Logger.root.onRecord.listen((record) {
     print('${record.level.name}: ${record.time}: ${record.message}');
   });
-
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
+
+  static MifareClassicTag? tag;
+
+  static final colorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255), brightness: Brightness.dark);
+  static final snackBarTheme =  const SnackBarThemeData(backgroundColor: Color.fromARGB(255, 0, 155, 202));
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MizipTools',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 255, 255, 255), brightness: Brightness.dark),
-        snackBarTheme: const SnackBarThemeData(backgroundColor: Color.fromARGB(255, 0, 155, 202)),
-        useMaterial3: true,
-      ),
+      theme: ThemeData(colorScheme: colorScheme, snackBarTheme: snackBarTheme, useMaterial3: true),
       home: MainPage(),
     );
   }
