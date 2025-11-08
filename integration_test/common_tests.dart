ae02a6c9a891867ec6c21f69813b7c0fe756674c
Laen <Laaen@hotmail.fr>
2025-11-08 21:12:14 +0100

    Ajout des derniers tests pour Mizip tags

---
+++ integration_test/common_tests.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:miziptools/extensions/uint8list_extensions.dart';
+import 'package:miziptools/main.dart';
+import 'package:miziptools/widgets/dump_tag.dart' show DumpTag;
+import 'package:path_provider/path_provider.dart';
+
+import 'full_app_test.dart' show generateMockMifareClassic;
+import 'mock_nfc_adapter.dart';
+import 'mock_nfc_tag.dart';
+
+Future<void> testDumpTag(WidgetTester tester, MockNfcTag mockTag) async{
+  final mockAdapter = MockNfcAdapter();
+  mockAdapter.setTag(mockTag);
+
+  await tester.pumpWidget(App(nfcAdapter: mockAdapter));
+  await tester.tap(find.widgetWithText(Tab, "Dumps"));
+  await tester.pumpAndSettle();
+  await tester.tap(find.widgetWithText(DumpTag, "Dump Tag"));
+  await tester.pumpAndSettle();
+  expect(find.widgetWithText(SnackBar, "Dump done file : ${mockTag.getUid().toUpperCase()}.dump"), findsOneWidget);
+      
+  // File exists
+  final dir = await getExternalStorageDirectory();
+  final file = dir!.listSync().first;
+  expect(file.path.split("/").last, equals("${mockTag.getUid().toUpperCase()}.dump"));
+
+  // Content is OK
+  final expectedContent = mockTag.data.map((block) => block.toHexString().toUpperCase()).join("\n");
+  final fileContent = File(file.path).readAsStringSync();
+  expect(fileContent, equals(expectedContent));
+}
+
+Future<void> testWriteFromDump(WidgetTester tester, MockNfcTag mockTag, String dumpContent) async{
+  final mockAdapter = MockNfcAdapter();
+  mockAdapter.setTag(mockTag);
+      
+  // Create the dump
+  final dir = await getExternalStorageDirectory();
+  File("${dir!.path}/${dumpContent.substring(0, 8)}.dump").writeAsStringSync(dumpContent);
+
+  await tester.pumpWidget(App(nfcAdapter: mockAdapter));
+  await tester.tap(find.widgetWithText(Tab, "Dumps"));
+  await tester.pumpAndSettle();
+  await tester.tap(find.byType(DropdownMenu));
+  await tester.pumpAndSettle();
+  await tester.tap(find.text("${dir.path}/${dumpContent.substring(0, 8)}.dump").last);
+  await tester.pumpAndSettle();
+  await tester.tap(find.widgetWithText(OutlinedButton, "Write"));
+  await tester.pumpAndSettle();
+  expect(find.widgetWithText(SnackBar, "Dump successfully written !"), findsOneWidget);
+
+  // Content is OK
+  expect(mockTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(dumpContent));
+}
+
+Future<void> testChangeUid(WidgetTester tester, MockNfcTag mockTag, String newUid, String expectedContent) async{
+  final mockAdapter = MockNfcAdapter();
+  mockAdapter.setTag(mockTag);
+
+  await tester.pumpWidget(App(nfcAdapter: mockAdapter));
+  await tester.tap(find.widgetWithText(Tab, "Advanced"));
+  await tester.pumpAndSettle();
+  await tester.enterText(find.byType(TextFormField), newUid);
+  await tester.tap(find.widgetWithText(OutlinedButton, "Ok"));
+  await tester.pumpAndSettle();
+  expect(find.widgetWithText(SnackBar, "UID changed successfully"), findsOneWidget);
+  expect(mockTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(expectedContent));
+}
--- integration_test/full_app_test.dart
+++ integration_test/full_app_test.dart
@@ -1,7 +1,4 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:miziptools/extensions/string_extensions.dart';
@@ -14,10 +11,12 @@ import 'package:miziptools/widgets/dump_tag.dart';
 import 'package:miziptools/widgets/tag_add_10.dart';
 import 'package:miziptools/widgets/tag_data.dart';
 import 'package:miziptools/widgets/write_from_dump.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dump_content.dart';
+import 'common_tests.dart';
+import 'expected_tag_content.dart';
 import 'mock_nfc_adapter.dart';
 import 'mock_nfc_tag.dart';
+
+
 
 void main(){
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -39,36 +38,11 @@ void main(){
   });
 
   group("MifareClassic tests", (){
-
-      final mockAdapter = MockNfcAdapter();
-      final mockMifareClassicTag = MockNfcTag(type: NfcTagType.mifareClassic,
-        data: [
-          "ED711B74F3890400C808002000000017".toUint8List(),
-          "6200488849884A884B88000000000000".toUint8List(),
-          "00000000000000000000000000000000".toUint8List(),
-          "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
-          "0E43004E61BC80010001000000005E02".toUint8List(),
-          "01000001000080010001000000008001".toUint8List(),
-          "AA020000000000000000000000000000".toUint8List(),
-          "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
-          "00A90AA3000000000000000000000006".toUint8List(),
-          "006C0E62000000000000000000000007".toUint8List(),
-          "55070000000000000000000000000000".toUint8List(),
-          "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
-          "00000000000000000000000000000000".toUint8List(),
-          "00000000000000000000000000000001".toUint8List(),
-          "55010000000000000000000000000000".toUint8List(),
-          "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
-          "00000000000000000000000000000000".toUint8List(),
-          "00000000000000000000000000000001".toUint8List(),
-          "55010000000000000000000000000000".toUint8List(),
-          "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
-        ] 
-      );
-      mockAdapter.setTag(mockMifareClassicTag);
-
     testWidgets("Start The app, and check displayed data in all menus", (tester) async {
 
+      final mockAdapter = MockNfcAdapter();
+      final mockMifareClassicTag = generateMockMifareClassic();
+      mockAdapter.setTag(mockMifareClassicTag);
       await tester.pumpWidget(App(nfcAdapter: mockAdapter));
 
       await tester.tap(find.widgetWithText(Tab, "Balance"));
@@ -88,87 +62,24 @@ void main(){
     });
 
     testWidgets("Test dump tag", (tester) async {
-      await tester.pumpWidget(App(nfcAdapter: mockAdapter));
-      await tester.tap(find.widgetWithText(Tab, "Dumps"));
-      await tester.pumpAndSettle();
-      await tester.tap(find.widgetWithText(DumpTag, "Dump Tag"));
-      await tester.pumpAndSettle();
-      expect(find.widgetWithText(SnackBar, "Dump done file : ${mockMifareClassicTag.getUid().toUpperCase()}.dump"), findsOneWidget);
-      
-      // File exists
-      final dir = await getExternalStorageDirectory();
-      final file = dir!.listSync().first;
-      expect(file.path.split("/").last, equals("${mockMifareClassicTag.getUid().toUpperCase()}.dump"));
-
-      // Content is OK
-      final expectedContent = mockMifareClassicTag.data.map((block) => block.toHexString().toUpperCase()).join("\n");
-      final fileContent = File(file.path).readAsStringSync();
-      expect(fileContent, equals(expectedContent));
+      await testDumpTag(tester, generateMockMifareClassic());
     });
 
     testWidgets("Test write from dump", (tester) async {
-      
-      // Create the dump
-      final dir = await getExternalStorageDirectory();
-      File("${dir!.path}/ABD453C7.dump").writeAsStringSync(dumpContentWriteFromDumpTest);
-
-      await tester.pumpWidget(App(nfcAdapter: mockAdapter));
-      await tester.tap(find.widgetWithText(Tab, "Dumps"));
-      await tester.pumpAndSettle();
-      await tester.tap(find.byType(DropdownMenu));
-      await tester.pumpAndSettle();
-      await tester.tap(find.text("${dir.path}/ABD453C7.dump").last);
-      await tester.pumpAndSettle();
-      await tester.tap(find.widgetWithText(OutlinedButton, "Write"));
-      await tester.pumpAndSettle();
-      expect(find.widgetWithText(SnackBar, "Dump successfully written !"), findsOneWidget);
-
-      // Content is OK
-      expect(mockMifareClassicTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(dumpContentWriteFromDumpTest));
+      await testWriteFromDump(tester, generateMockMifareClassic(), dumpContentWriteFromDumpTest);
     });
 
     testWidgets("Test change UID", (tester) async {
-      final newUid = "ABD453C7";
-      await tester.pumpWidget(App(nfcAdapter: mockAdapter));
-      await tester.tap(find.widgetWithText(Tab, "Advanced"));
-      await tester.pumpAndSettle();
-      await tester.enterText(find.byType(TextFormField), newUid);
-      await tester.tap(find.widgetWithText(OutlinedButton, "Ok"));
-      await tester.pumpAndSettle();
-      expect(find.widgetWithText(SnackBar, "UID changed successfully"), findsOneWidget);
-      expect(mockMifareClassicTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(expectedTagContentChangeUidTestMifareClassic));
+      await testChangeUid(tester, generateMockMifareClassic(), "ABD453C7", expectedTagContentChangeUidTestMifareClassic);
     });
   });
 
   group("MiZip tests", (){
-    final mockAdapter = MockNfcAdapter();
-    final mockMizipTag = MockNfcTag(type: NfcTagType.mifareClassic,
-      data: [
-        "ED711B74F3890400C808002000000017".toUint8List(),
-        "6200488849884A884B88000000000000".toUint8List(),
-        "00000000000000000000000000000000".toUint8List(),
-        "A0A1A2A3A4A5787788C1B4C123439EEF".toUint8List(),
-        "0E43004E61BC80010001000000005E02".toUint8List(),
-        "01000001000080010001000000008001".toUint8List(),
-        "AA020000000000000000000000000000".toUint8List(),
-        "E4634151649478778803EA586922C355".toUint8List(),
-        "00A90AA3000000000000000000000006".toUint8List(),
-        "006C0E62000000000000000000000007".toUint8List(),
-        "55070000000000000000000000000000".toUint8List(),
-        "4604D2437F5E787788126893748F2935".toUint8List(),
-        "00000000000000000000000000000000".toUint8List(),
-        "00000000000000000000000000000001".toUint8List(),
-        "55010000000000000000000000000000".toUint8List(),
-        "0F035ADBC17878778800B139FE074DDA".toUint8List(),
-        "00000000000000000000000000000000".toUint8List(),
-        "00000000000000000000000000000001".toUint8List(),
-        "55010000000000000000000000000000".toUint8List(),
-        "DC0BAC5BA9E178778800AB67CA563689".toUint8List(),
-      ] 
-    );
-    mockAdapter.setTag(mockMizipTag);
-
     testWidgets("Start The app, and check displayed data in all menus", (tester) async {
+
+      final mockAdapter = MockNfcAdapter();
+      final mockMizipTag = generateMockMizipTag();
+      mockAdapter.setTag(mockMizipTag);
 
       await tester.pumpWidget(App(nfcAdapter: mockAdapter));
 
@@ -194,6 +105,11 @@ void main(){
     });
 
     testWidgets("Change balance", (tester) async{
+
+      final mockAdapter = MockNfcAdapter();
+      final mockMizipTag = generateMockMizipTag();
+      mockAdapter.setTag(mockMizipTag);
+
       final newBalance = "26.92";
       await tester.pumpWidget(App(nfcAdapter: mockAdapter));
       await tester.tap(find.widgetWithText(Tab, "Balance"));
@@ -205,5 +121,89 @@ void main(){
       expect(mockMizipTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(expectedTagContentChangeBalanceTest));
 
     });
+
+    testWidgets("Add 10\$", (tester) async{
+
+      final mockAdapter = MockNfcAdapter();
+      final mockMizipTag = generateMockMizipTag();
+      mockAdapter.setTag(mockMizipTag);
+
+      await tester.pumpWidget(App(nfcAdapter: mockAdapter));
+      await tester.tap(find.widgetWithText(Tab, "Balance"));
+      await tester.pumpAndSettle();
+      await tester.tap(find.widgetWithText(OutlinedButton, "Add 10\$")); 
+      await tester.pumpAndSettle();
+      expect(find.widgetWithText(SnackBar, "Balance changed successfully"), findsOneWidget);
+      expect(mockMizipTag.data.map((block) => block.toHexString().toUpperCase()).join("\n"), equals(expectedTagContentAddTenTest));
+
+    });
+
+    testWidgets("Test dump tag", (tester) async {
+      await testDumpTag(tester, generateMockMizipTag());
+    });
+
+    testWidgets("Test write from dump", (tester) async {
+      await testWriteFromDump(tester, generateMockMizipTag(), dumpContentWriteFromDumpTest);
+    });
+
+    testWidgets("Test change UID", (tester) async {
+      await testChangeUid(tester, generateMockMizipTag(), "ABD453C7", expectedTagCOntentChangeUidTestMizip);
+    });
+
   });
+}
+
+
+MockNfcTag generateMockMifareClassic(){
+  return MockNfcTag(type: NfcTagType.mifareClassic,
+    data: [
+      "ED711B74F3890400C808002000000017".toUint8List(),
+      "6200488849884A884B88000000000000".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
+      "0E43004E61BC80010001000000005E02".toUint8List(),
+      "01000001000080010001000000008001".toUint8List(),
+      "AA020000000000000000000000000000".toUint8List(),
+      "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
+      "00A90AA3000000000000000000000006".toUint8List(),
+      "006C0E62000000000000000000000007".toUint8List(),
+      "55070000000000000000000000000000".toUint8List(),
+      "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "00000000000000000000000000000001".toUint8List(),
+      "55010000000000000000000000000000".toUint8List(),
+      "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "00000000000000000000000000000001".toUint8List(),
+      "55010000000000000000000000000000".toUint8List(),
+      "FFFFFFFFFFFF78778800FFFFFFFFFFFF".toUint8List(),
+    ] 
+  );
+}
+
+MockNfcTag generateMockMizipTag(){
+  return MockNfcTag(type: NfcTagType.mifareClassic,
+    data: [
+      "ED711B74F3890400C808002000000017".toUint8List(),
+      "6200488849884A884B88000000000000".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "A0A1A2A3A4A5787788C1B4C123439EEF".toUint8List(),
+      "0E43004E61BC80010001000000005E02".toUint8List(),
+      "01000001000080010001000000008001".toUint8List(),
+      "AA020000000000000000000000000000".toUint8List(),
+      "E4634151649478778803EA586922C355".toUint8List(),
+      "00A90AA3000000000000000000000006".toUint8List(),
+      "006C0E62000000000000000000000007".toUint8List(),
+      "55070000000000000000000000000000".toUint8List(),
+      "4604D2437F5E787788126893748F2935".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "00000000000000000000000000000001".toUint8List(),
+      "55010000000000000000000000000000".toUint8List(),
+      "0F035ADBC17878778800B139FE074DDA".toUint8List(),
+      "00000000000000000000000000000000".toUint8List(),
+      "00000000000000000000000000000001".toUint8List(),
+      "55010000000000000000000000000000".toUint8List(),
+      "DC0BAC5BA9E178778800AB67CA563689".toUint8List(),
+    ] 
+  );
 }
