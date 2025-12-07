7343798fd379c781610afbc919d1d6e780933d1a
Laen <Laaen@hotmail.fr>
2025-12-07 23:07:57 +0100

    Fix error message display

--- lib/exceptions/nfc_exception_handler.dart
+++ lib/exceptions/nfc_exception_handler.dart
@@ -1,34 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:miziptools/exceptions/nfc_exceptions.dart';
+import 'package:miziptools/misc/snackbar.dart';
 
 class NfcExceptionHandler {
 
   static void handleException(Exception e, BuildContext context){
     switch(e.runtimeType){
-      case ReadTagRemovedException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while reading : Tag was removed");
-      case ReadSectorAuthenticationFailed _:
-        NfcExceptionHandler.showSnackbar(context, "Error while reading : Incorrect key");
-      case ReadRetriesExcedeedException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while reading : Maximum number of retries reached");
-      case ReadUnknownException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while reading : Unknow error");
-      case WriteTagRemovedException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while writing : Tag was removed");
-      case WriteSectorAuthenticationFailed _:
-        NfcExceptionHandler.showSnackbar(context, "Error while writing : Incorrect key");
-      case WriteRetriesExcedeedException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while writing : Maximum number of retries reached");
-      case WriteUnknownException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while writing : Unknow error");
-      case ReleaseFailedException _:
-        NfcExceptionHandler.showSnackbar(context, "Error while releasing the tag : Tag was lost");
+      case const (ReadTagRemovedException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while reading : Tag was removed");
+      case const (ReadSectorAuthenticationFailed):
+        NfcExceptionHandler.displaySnackbar(context, "Error while reading : Incorrect key");
+      case const (ReadRetriesExcedeedException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while reading : Maximum number of retries reached");
+      case const (ReadUnknownException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while reading : Unknow error");
+      case const (WriteTagRemovedException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while writing : Tag was removed");
+      case const (WriteSectorAuthenticationFailed):
+        NfcExceptionHandler.displaySnackbar(context, "Error while writing : Incorrect key");
+      case const (WriteRetriesExcedeedException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while writing : Maximum number of retries reached");
+      case const (WriteUnknownException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while writing : Unknow error");
+      case const (ReleaseFailedException):
+        NfcExceptionHandler.displaySnackbar(context, "Error while releasing the tag : Tag was lost");
     }
   }
 
-  static void showSnackbar(BuildContext context, String message){
+  static void displaySnackbar(BuildContext context, String message){
     if(context.mounted){
-      showSnackbar(context, message);
+      showSnackBar(context, message);
     }
   }
 
