0b0d3d1516dc6c02f3ce42b82a2808f7db047e88
Laen <Laaen@hotmail.fr>
2026-07-04 10:19:25 +0000

    Test sonarqube

--- lib/data_dir/data_dir.dart
+++ lib/data_dir/data_dir.dart
@@ -3,8 +3,9 @@ import "package:flutter/material.dart";
 
 /// Class used for interacting with the data folder on the phone
 /// It is used as a global object allowing operations on the data folder
+/// All methods do their work in the dataDir folder
 class DataDir with ChangeNotifier {
-  /// Initializes a DataDir object, must be called with where you want to do stuff
+  /// Initializes a DataDir object, must be called with where you want to work
   /// In our case it's Android/data/com.laen.miziptools_v2/files/
   DataDir({required this.dataDir});
 
@@ -12,25 +13,31 @@ class DataDir with ChangeNotifier {
   final Directory dataDir;
 
   /// Writes the provided content to the file
-  Future<void> writeFile(final String fileName, final String content) async {
-    final IOSink file = File("${dataDir.path}/$fileName").openWrite();
-    file.write(content);
+  /// Triggers a listener notification in order to reload the UI
+  Future<void> writeFile(String fileName, String content) async {
+    final IOSink file = File("${dataDir.path}/$fileName").openWrite()
+      ..write(content);
     await file.close();
     notifyListeners();
   }
 
+  /// Returns the content of the file
   String readFile(String fileName) {
     return File("${dataDir.path}/$fileName").readAsStringSync();
   }
 
+  /// Returns a list of [FileSystemEntity] present in the folder
   List<FileSystemEntity> getFilesList() {
     return dataDir.listSync();
   }
 
+  /// Returns a list of [FileSystemEntity] of the tag dumps
   List<FileSystemEntity> getDumpsList() {
     return dataDir
         .listSync()
-        .where((file) => file.path.split("/").last != "uid_save")
+        .where(
+          (FileSystemEntity file) => file.path.split("/").last != "uid_save",
+        )
         .toList();
   }
 }
