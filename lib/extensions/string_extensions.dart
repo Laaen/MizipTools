40a539830f4e2034a2d101f5807af59f2b7a0f43
Laen <Laaen@hotmail.fr>
2026-07-04 23:04:14 +0000

    Lint for extensions folder

--- lib/extensions/string_extensions.dart
+++ lib/extensions/string_extensions.dart
@@ -1,20 +1,36 @@
-import 'dart:typed_data';
-import 'package:collection/collection.dart';
+import "dart:typed_data";
+import "package:collection/collection.dart";
 
-class ConversionError implements Exception{
-  String cause;
+/// Thrown if something bad happens during the conversion
+/// from String to Uint8List
+class ConversionError implements Exception {
+  /// Create a [ConversionError] with the given cause
   ConversionError(this.cause);
+
+  /// Why the exception was thrown
+  String cause;
 }
 
-extension Converter on String{
-
-  Uint8List toUint8List(){
-    if(length % 2 != 0){
+/// This extension contains only one function to convert a String to a Uint8List
+extension Converter on String {
+  /// Returns a Uint8List from a string
+  /// Throws a [ConversionError] if an incorrect string is passed to it
+  /// Ex : "0A67B9" => [0x0A, 0x67, 0xB9]
+  Uint8List toUint8List() {
+    if (length % 2 != 0) {
       throw ConversionError("Odd number of characters");
-    } else if(length < 2){
+    } else if (length < 2) {
       throw ConversionError("Not enough characters (need at least 2)");
     }
-    return Uint8List.fromList(split("").slices(2).map((x) => int.parse(x.join(), radix: 16)).toList());
+    try {
+      return Uint8List.fromList(
+        split("").slices(2).map((x) => int.parse(x.join(), radix: 16)).toList(),
+      );
+    } on FormatException {
+      throw ConversionError(
+        "A non-hexadecimal chaeacter is present in the string",
+      );
+    }
   }
-
 }
+
--- lib/extensions/uint8list_extensions.dart
+++ lib/extensions/uint8list_extensions.dart
@@ -1,7 +1,11 @@
-import 'dart:typed_data';
+import "dart:typed_data";
 
-extension Converter on Uint8List{
-  String toHexString(){
-    return map((x) => x.toRadixString(16).padLeft(2, "0")).toList().join("");
+/// This extension contains only one function to convert a Uint8List to a String
+extension Converter on Uint8List {
+  /// Returns a String from a Uint8List
+  /// Ex: [0x0A, 0x67, 0xB9] => "0A67B9"
+  String toHexString() {
+    return map((x) => x.toRadixString(16).padLeft(2, "0")).toList().join();
   }
 }
+
