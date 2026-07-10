acee99f17253f54165ec4b3fb655bb9746d568ff
Laen <Laaen@hotmail.fr>
2026-07-10 20:25:05 +0000

    Lint for nfc_tag

--- lib/nfc/nfc_tag.dart
+++ lib/nfc/nfc_tag.dart
@@ -1,12 +1,23 @@
-enum NfcTagType{
+import "package:flutter_nfc_kit/flutter_nfc_kit.dart";
+
+/// Type of the tag
+enum NfcTagType {
+  /// Mifare classic
   mifareClassic,
+
+  /// Something else (not supported)
   other
 }
 
+/// A NFC tag
 class NfcTag {
-  final NfcTagType type;
-  final String id;
-
+  /// Creates a new [NFCTag] from the given type and id
   NfcTag({required this.type, required this.id});
 
+  /// Which type it is
+  final NfcTagType type;
+
+  /// Its UID
+  final String id;
 }
+
