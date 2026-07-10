9f085ac7c07441b1247b7e8e8b5bf79dde1801cc
Laen <Laaen@hotmail.fr>
2026-07-10 20:52:24 +0000

    Lint for advanced_menu

--- lib/pages/advanced_menu.dart
+++ lib/pages/advanced_menu.dart
@@ -1,12 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:miziptools/nfc/currentnfctag.dart';
-import 'package:miziptools/widgets/advanced/auto_repair.dart';
-import 'package:miziptools/widgets/advanced/change_uid.dart';
-import 'package:miziptools/widgets/common/tag_data.dart';
-import 'package:provider/provider.dart';
+import "package:flutter/material.dart";
+import "package:miziptools/nfc/currentnfctag.dart";
+import "package:miziptools/widgets/advanced/auto_repair.dart";
+import "package:miziptools/widgets/advanced/change_uid.dart";
+import "package:miziptools/widgets/common/tag_data.dart";
+import "package:provider/provider.dart";
 
-class AdvancedMenu extends StatelessWidget{
-
+/// Page for advanced options
+///
+/// Change UID => Tag is present
+/// Auto-repair => Tag is present and is not Mizip
+class AdvancedMenu extends StatelessWidget {
+  /// Creates an [AdvancedMenu]
   const AdvancedMenu({super.key});
 
   @override
@@ -14,11 +18,11 @@ class AdvancedMenu extends StatelessWidget{
     final tag = context.read<CurrentNFCTag>();
     return ListView(
       children: [
-        TagData(),
+        const TagData(),
         if (tag.isPresent()) ChangeUid(),
-        if (tag.isPresent() && !tag.isMizipTag()) AutoRepair()
+        if (tag.isPresent() && !tag.isMizipTag()) AutoRepair(),
       ],
     );
   }
-
 }
+
