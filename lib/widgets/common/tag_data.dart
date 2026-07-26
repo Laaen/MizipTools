0f2686b043e422531330030237ae6299f6856eef
Laen <Laaen@hotmail.fr>
2026-07-26 15:40:51 +0000

    Lint for tag_data

--- lib/widgets/common/tag_data.dart
+++ lib/widgets/common/tag_data.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:miziptools/extensions/uint8list_extensions.dart';
-import 'package:miziptools/nfc/currentnfctag.dart';
-import 'package:miziptools/tags/balance.dart';
-import 'package:miziptools/widgets/basic/container_with_border.dart';
-import 'package:provider/provider.dart';
+import "package:flutter/material.dart";
+import "package:miziptools/extensions/uint8list_extensions.dart";
+import "package:miziptools/nfc/currentnfctag.dart";
+import "package:miziptools/tags/balance.dart";
+import "package:miziptools/widgets/basic/container_with_border.dart";
+import "package:provider/provider.dart";
 
+/// Widget used to display TagData
 class TagData extends StatelessWidget {
+  /// Returns a new [TagData]
   const TagData({super.key});
 
   @override
@@ -15,41 +17,60 @@ class TagData extends StatelessWidget {
     if (tag.isPresent()) {
       return ContainerWithBorder(child: getTagDataDisplay(tag));
     } else {
-      return ContainerWithBorder(
-          child: Text("No tag detected", style: TextStyle(fontSize: 16)));
+      return const ContainerWithBorder(
+        child: Text("No tag detected", style: TextStyle(fontSize: 16)),
+      );
     }
   }
 
+  /// The widget which displays the data
   Widget getTagDataDisplay(CurrentNFCTag tag) {
-    return Column(children: [
-      Text(
-        "Tag data",
-        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
-      ),
-      Container(
-        height: 10,
-      ),
-      if (!tag.isMizipTag())
-        Row(children: [
-          Text("Not a MiZip tag (Mifare Classic)",
-              style: TextStyle(fontSize: 16))
-        ]),
-      Row(children: [
-        Text("UID: ${tag.getUid().toHexString().toUpperCase()}",
-            style: TextStyle(fontSize: 16))
-      ]),
-      if (tag.isMizipTag())
-        Row(children: [
-          Text("Balance: ${tag.getBalance().getStringBalance()}\$",
-              style: TextStyle(fontSize: 16))
-        ]),
-      if (tag.isMizipTag() && tag.getBalance().valid != BalanceValidity.valid)
-        Row(children: [
-          Text(
-            "Invalid checksum",
-            style: TextStyle(fontSize: 18, color: Colors.red),
-          )
-        ])
-    ]);
+    return Column(
+      children: [
+        const Text(
+          "Tag data",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+        ),
+        // Empty container for some spacing
+        Container(
+          height: 10,
+        ),
+        if (!tag.isMizipTag())
+          const Row(
+            children: [
+              Text(
+                "Not a MiZip tag (Mifare Classic)",
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        Row(
+          children: [
+            Text(
+              "UID: ${tag.getUid().toHexString().toUpperCase()}",
+              style: const TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+        if (tag.isMizipTag())
+          Row(
+            children: [
+              Text(
+                "Balance: ${tag.getBalance().getStringBalance()}\$",
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        if (tag.isMizipTag() && tag.getBalance().valid != BalanceValidity.valid)
+          const Row(
+            children: [
+              Text(
+                "Invalid checksum",
+                style: TextStyle(fontSize: 18, color: Colors.red),
+              ),
+            ],
+          ),
+      ],
+    );
   }
 }
